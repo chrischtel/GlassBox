3cdf9f3c5ef36a1acb112829d94e9f058ff5e0bc
Christian Brendlin <ch@brendlinonline.de>
2025-09-07 13:07:40 +0200

    Add project files.

---
+++ GlassBox.cpp
@@ -0,0 +1,12 @@
+﻿// GlassBox.cpp : Defines the entry point for the application.
+//
+
+#include "GlassBox.h"
+
+using namespace std;
+
+int main()
+{
+	println("Hello, GlassBox!");
+	return 0;
+}
---
+++ GlassBox.h
@@ -0,0 +1,9 @@
+﻿// GlassBox.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <print>
+
+// TODO: Reference additional headers your program requires here.
